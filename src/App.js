6e65ac53c3c46ddd5b960865c019879320468e84
Elyes-Lounas <121710591+Elyes-Lounas@users.noreply.github.com>
2024-03-14 02:55:20 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import RandomQuotes from './components/randomQuotes/RandomQuotes'
+import Main from "./components/RecepeApp/Main";
+
 function App() {
   return (
     <>
-      <RandomQuotes />
+      <Main />
     </>
   );
 }
